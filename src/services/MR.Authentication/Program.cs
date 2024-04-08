b8409f837d85071344869da1587d55dc3ce19e54
RodineiRiboli <rodineiriboli@hotmail.com>
2024-04-07 23:28:41 -0300

    criação e configuração de arquivo docker compose e dockerfile de todos projetos necessários

--- src/services/MR.Authentication/Program.cs
+++ src/services/MR.Authentication/Program.cs
@@ -7,6 +7,12 @@ namespace MR.Authentication
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Configuration
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
